d766074589614db1c0f922ccf4bfd66b9173fa31
matthogan <iammatthewhogan@gmail.com>
2015-10-16 02:02:47 +0100

    Initial

---
+++ src/main/java/com/codejago/jbehave/spring/sample/steps/MySteps.java
@@ -0,0 +1,28 @@
+package com.codejago.jbehave.spring.sample.steps;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+public class MySteps {
+
+    @Given("I am a pending step")
+    public void a() {
+
+    }
+
+    @Then("And I am still pending step")
+    public void b() {
+
+    }
+
+    @When("a good soul will implement me")
+    public void c() {
+
+    }
+
+    @Then("I shall be happy")
+    public void d() {
+
+    }
+}
